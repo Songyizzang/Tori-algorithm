f3885be7f3f78bd43b86ff5bee281385366614fe
Yeseul Choi <seulshine@gmail.com>
2021-01-17 21:40:33 +0900

    Update 알파벳_비트연산자.py

--- 6주차 Recursion_Stack/형진/알파벳_비트연산자.py
+++ 6주차 Recursion_Stack/형진/알파벳_비트연산자.py
@@ -14,6 +14,8 @@ sys.stdin = open("input.txt", "r")
 row, col = map(int, input().split())
 board = [list(input()) for _ in range(row)]
 visited = [[0] * col for _ in range(row)]
+#visited 의 역할은 동일 depth에서(BFS에서 동일 depth) 같은 경로(CAB = BAC) 인 경우는 뭘 하든 개수가 같기 때문에 미리 쳐냅니다.
+#안 그러면 메모리 초과가 납니다!!
 for i in range(row):
     for j in range(col):
         val = ord(board[i][j]) - 65
